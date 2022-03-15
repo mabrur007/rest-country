d099fe5819e4589f2f2be16015ad395d5b6ef8de
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-15 16:47:29 +0600

    final commit

--- countries.js
+++ countries.js
@@ -4,18 +4,19 @@ const loadCountries = () => {
         .then(data => displayCountries(data));
 }
 const displayCountries = countries => {
-    // console.log(countries[2]);
+    console.log(countries[2]);
     const countriesHTML = countries.map(country => getCountryHTML(country));
     // console.log(countriesHTML);
     const container = document.getElementById('countries');
-    container.innerHTML = countriesHTML;
+    container.innerHTML = countriesHTML.join(' ');
 }
 const getCountryHTML = country => {
     return `
-        <div>
+        <div class="country">
             <h2>${country.name.common}</h2>
+            <img src="${country.flags.png}">
         </div>
-    `
+    `;
 }
 
 loadCountries();
