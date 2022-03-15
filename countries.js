815c137e21046151a5ece63a7dc5cf074d200082
mabrur007 <mirmd.mabrur@gmail.com>
2022-03-15 15:51:31 +0600

    countries showing in UI

--- countries.js
+++ countries.js
@@ -6,7 +6,9 @@ const loadCountries = () => {
 const displayCountries = countries => {
     // console.log(countries[2]);
     const countriesHTML = countries.map(country => getCountryHTML(country));
-    console.log(countriesHTML);
+    // console.log(countriesHTML);
+    const container = document.getElementById('countries');
+    container.innerHTML = countriesHTML;
 }
 const getCountryHTML = country => {
     return `
